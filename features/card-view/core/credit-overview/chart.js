9841018058abaa9872d97f7953be95b8b87661de
nertc <davidtsiklauri7@gmail.com>
2020-12-30 13:35:01 +0400

    feat(card-view credit-overview): Finish Credit Overview component

--- features/card-view/core/credit-overview/chart.js
+++ features/card-view/core/credit-overview/chart.js
@@ -1,18 +1,15 @@
 let svgns = "http://www.w3.org/2000/svg";
-const r = 30, size = 100;
+const r = 24, size = 100, strokeWidth = 39, hoverWidth = 50;
 
 function createChart( dataset, colors, bg ) {
     const sum = dataset.reduce((prev, cur) => prev + cur, 0);
 
     const chart = document.createElementNS(svgns, 'svg');
-    chart.setAttributeNS(null, 'width', "100%");
-    chart.setAttributeNS(null, 'height', "100%");
     chart.setAttributeNS(null, 'viewBox', `0 0 ${size} ${size}`);
 
     let offset = 0;
     dataset.forEach( (data, index) => {
         offset += data;
-        let strokeWidth = 10;
         const element = createCircle({
         'fill': 'transparent',
         'stroke': colors[index],
@@ -21,8 +18,8 @@ function createChart( dataset, colors, bg ) {
         'stroke-dashoffset': 2 * Math.PI * r / sum * offset
         });
         element.addEventListener('mouseover', mouse => {
-            mouse.currentTarget.setAttributeNS(null, 'stroke-width', 20);
-            pop(mouse.x, mouse.y)
+            mouse.currentTarget.setAttributeNS(null, 'stroke-width', hoverWidth);
+            pop(mouse.x, mouse.y, dataset[index])
         });
         element.addEventListener('mouseout', mouse => {
             mouse.currentTarget.setAttributeNS(null, 'stroke-width', strokeWidth);
@@ -33,12 +30,14 @@ function createChart( dataset, colors, bg ) {
 
     const center = createCircle({'fill': bg});
     chart.appendChild(center);
-    console.log(chart);
-    document.getElementsByClassName('credit-overview')[0].appendChild(chart);
+    chart.classList.add('pie');
+    document.getElementsByClassName('credit-overview')[0]
+        .getElementsByClassName('chart')[0].appendChild(chart);
 }
 
-function pop( x, y ) {
+function pop( x, y, amount ) {
     const popup = document.getElementById('popup');
+    popup.getElementsByTagName('span')[0].innerText = `$${amount.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".")}`;
     popup.style.display = 'block';
     popup.style.left = x - popup.offsetWidth / 2;
     popup.style.top = y - popup.offsetHeight - 10;
