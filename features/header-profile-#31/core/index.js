6382d1da236a7b8a39dbe39c61cbfc9efeb6234e
user1 <sesili.lomadze.1@btu.edu.ge>
2020-12-30 16:52:20 +0400

    fix4 feature header-profile#31 - fixed fonts, improfed onClick function

--- features/header-profile-#31/core/index.js
+++ features/header-profile-#31/core/index.js
@@ -1,11 +1,14 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-window.onclick = function (e) {
+document.onclick = function (e) {
     if (!e.target.matches('.dropbtn')) {
         var myDropdown = document.getElementById("myDropdown");
         if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
+                myDropdown.classList.remove('show');
         }
     }
 }
+  document.getElementById("myDropdown").addEventListener('click',function(event){
+    event.stopPropagation();
+});
