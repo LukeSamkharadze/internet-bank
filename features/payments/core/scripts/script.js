71cbb6f5f073b3ebbad933ee010f71cc4aa28e0c
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-01-04 02:21:14 +0400

    feat(feat/payments:): finish providers section

---
+++ features/payments/core/scripts/script.js
@@ -0,0 +1,36 @@
+const providers = [...document.getElementsByClassName("provider")];
+const providerWrappers = [
+    ...document.getElementsByClassName("provider-wrapper"),
+];
+
+// adds commas after every provider copmany's name. (except last one)
+function addCommas() {
+    providers.forEach((provider) => {
+        let providerNames = [...provider.querySelectorAll("p")];
+        providerNames.forEach((p, index) => {
+            if (providerNames.length != index + 1) {
+                p.innerText += ",";
+            }
+        });
+    });
+}
+
+// moving between providers.
+function changeProvider(selectedProviderWrapper) {
+    if (selectedProviderWrapper.classList.contains("selected-provider")) {
+        return;
+    }
+
+    document
+        .querySelector(".selected-provider")
+        .classList.remove("selected-provider");
+
+    selectedProviderWrapper.classList.add("selected-provider");
+}
+
+addCommas();
+providerWrappers.forEach((providerWrapper) => {
+    providerWrapper.addEventListener("click", () => {
+        changeProvider(providerWrapper);
+    });
+});
