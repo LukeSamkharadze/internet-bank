0bac1d875321677dc71eefdf605ab5923e4f0842
unknown <gurammateshvili3@gmail.com>
2021-01-09 15:56:25 +0400

    fix: content height

--- layout/layoutwemptydashboard/index.js
+++ layout/layoutwemptydashboard/index.js
@@ -5,3 +5,19 @@ burgerMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('menu--active');
     burgerMenu.classList.toggle('burger-menu--active');
 });
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+document.onclick = function (e) {
+    if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+        }
+    }
+}
+  document.getElementById("myDropdown").addEventListener('click',function(event){
+    event.stopPropagation();
+});
