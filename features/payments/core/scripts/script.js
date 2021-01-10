68756223d83dd018ad4735908edd1fccee355191
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-01-10 16:36:49 +0400

    feat(feat/payments): added small and slow search function

--- features/payments/core/scripts/script.js
+++ features/payments/core/scripts/script.js
@@ -1,8 +1,62 @@
 const providers = [...document.getElementsByClassName("provider")];
+
 const providerWrappers = [
     ...document.getElementsByClassName("provider-wrapper"),
 ];
 const payments = [...document.getElementsByClassName("payment")];
+
+const searchInput = document.getElementById("find-provider-input");
+
+const providerCompanies = new Map();
+
+providerWrappers.forEach((providerWrapper) => {
+    const names = providerWrapper.querySelectorAll("p");
+    providerCompanies.set(providerWrapper.dataset.index, [...names]);
+});
+
+function search(text) {
+    //  tavidan yvelas order iqneba 1
+    providerWrappers.forEach((providerWrapper) => {
+        providerWrapper.style.order = "1";
+    });
+    text = text.toLowerCase();
+    let maxcount = 0;
+    let bestMatchIndex;
+    let isCorrect = false; // tu bevri aso emtxveva mara 1 aso mainc ari ucxo, mashin ar vtvli.
+    for (prov of providerCompanies) {
+        const arrayOfProviders = prov[1]; // mapshi 1 indexze ari p tagebis array.
+        arrayOfProviders.forEach((providerName) => {
+            providerName = providerName.innerText.toLowerCase();
+            let count = 0;
+            if (providerName.includes(text)) {
+                isCorrect = true;
+            }
+            for (let i = 0; i < text.length; i++) {
+                if (text[i] == providerName[i]) {
+                    count++;
+                } else {
+                    continue;
+                }
+            }
+            if (count > maxcount) {
+                maxcount = count;
+                bestMatchIndex = [...prov[0]];
+                bestMatchWord = providerName;
+            }
+        });
+    }
+
+    if (maxcount >= 1 && isCorrect) {
+        console.log(bestMatchIndex);
+        providerWrappers[bestMatchIndex].style.order = "-1";
+    } else {
+        providerWrappers.forEach((providerWrapper) => {
+            providerWrapper.style.order = "1";
+        });
+    }
+}
+
+//  aq iwyeba dzveli kodi
 
 // adds commas after every provider copmany's name. (except last one)
 function addCommas() {
@@ -43,6 +97,12 @@ function main() {
         providerWrapper.addEventListener("click", () => {
             changeProvider(providerWrapper);
         });
+    });
+
+    searchInput.addEventListener("keydown", () => {
+        setTimeout(() => {
+            search(searchInput.value);
+        }, 0);
     });
 }
 
