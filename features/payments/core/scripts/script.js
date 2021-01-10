12ff3caa878e24137e728511b739db7254da3b6f
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-01-10 15:40:14 +0400

    feat(feat/payments): finish except dropdowns & pixelPerfect

--- features/payments/core/scripts/script.js
+++ features/payments/core/scripts/script.js
@@ -2,6 +2,7 @@ const providers = [...document.getElementsByClassName("provider")];
 const providerWrappers = [
     ...document.getElementsByClassName("provider-wrapper"),
 ];
+const payments = [...document.getElementsByClassName("payment")];
 
 // adds commas after every provider copmany's name. (except last one)
 function addCommas() {
@@ -25,7 +26,14 @@ function changeProvider(selectedProviderWrapper) {
         .querySelector(".selected-provider")
         .classList.remove("selected-provider");
 
+    document
+        .querySelector(".current-payment")
+        .classList.remove("current-payment");
+
     selectedProviderWrapper.classList.add("selected-provider");
+
+    let index = selectedProviderWrapper.dataset.index;
+    payments[index].classList.add("current-payment");
 }
 
 function main() {
