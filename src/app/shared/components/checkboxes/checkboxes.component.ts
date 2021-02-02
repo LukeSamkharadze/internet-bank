fc0af02accfe4a43cc44148c93ce1c36990d260d
user1 <62304291+Qrisno@users.noreply.github.com>
2021-02-03 00:32:18 +0400

    added readme and interpolation

--- src/app/shared/components/checkboxes/checkboxes.component.ts
+++ src/app/shared/components/checkboxes/checkboxes.component.ts
@@ -3,16 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkboxes',
   templateUrl: './checkboxes.component.html',
-  styleUrls: ['./checkboxes.component.scss']
+  styleUrls: ['./checkboxes.component.scss'],
 })
 export class CheckboxesComponent implements OnInit {
-  @Input() checkboxType:string;
-  @Input() checked:boolean;
-  @Input() disabled:boolean;
-  @Input() checkboxError:string;
-  constructor() { }
+  @Input() checkboxType: string;
+  @Input() checked: boolean;
+  @Input() disabled: boolean;
+  @Input() checkboxError: string;
+  @Input() checkboxText!: string;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
