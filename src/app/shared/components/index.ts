7d1e534ac29550044747949932d4f969015390fd
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-02-02 18:48:51 +0400

    fix(component/textarea): added component export to index.ts

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,7 @@
 import { InputComponent } from './input/input.component';
+import { TextareaComponent } from './textarea/textarea.component';
 
 export * from './input/input.component';
+export * from './textarea/textarea.component';
 
-export const components = [InputComponent];
+export const components = [InputComponent, TextareaComponent];
