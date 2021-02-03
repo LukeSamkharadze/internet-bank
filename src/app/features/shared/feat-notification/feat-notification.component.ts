0548a43a850185cc77a6790648648a83b352f12f
user2 <aslanioniani2000@gmail.com>
2021-02-03 17:58:33 +0400

    feat(notifications): Creating Component

---
+++ src/app/features/shared/feat-notification/feat-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feat-notification',
+  templateUrl: './feat-notification.component.html',
+  styleUrls: ['./feat-notification.component.scss'],
+})
+export class FeatNotificationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/features/shared/features-shared.module.ts
+++ src/app/features/shared/features-shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { FeatNotificationComponent } from './feat-notification/feat-notification.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FeatNotificationComponent],
+  imports: [CommonModule],
 })
-export class FeaturesSharedModule { }
+export class FeaturesSharedModule {}
