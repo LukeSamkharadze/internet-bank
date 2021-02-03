21b75228724ae704a93e9a069c2421d1bdd7e5e7
mariam-ts <tsirekidze.ma@gmail.com>
2021-02-03 16:31:25 +0400

    addToggleComponent

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,7 @@
 import { InputComponent } from './input/input.component';
+import { ToggleComponent } from './toggle/toggle.component';
 
 export * from './input/input.component';
+export * from './toggle/toggle.component';
 
-export const components = [InputComponent];
+export const components = [InputComponent, ToggleComponent];
---
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.scss'],
+})
+export class ToggleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
