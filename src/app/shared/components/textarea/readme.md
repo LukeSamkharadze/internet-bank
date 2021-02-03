d1a5461123544c9df4b5ac146a12901a7a679dc3
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-02-04 02:26:30 +0400

    fix(component/textarea): rewrote textarea using Control value Accessor

---
+++ src/app/shared/components/textarea/readme.md
@@ -0,0 +1,3 @@
+To use textarea in your form, simply implement it like this:
+
+`<app-textarea formControlName='nameGoesHere' [placeholder]='your Placeholder'></app-textarea>`
--- src/app/shared/components/textarea/textarea.component.ts
+++ src/app/shared/components/textarea/textarea.component.ts
@@ -1,14 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl, FormControl } from '@angular/forms';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-textarea',
   templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaComponent),
+      multi: true,
+    },
+  ],
 })
-export class TextareaComponent implements OnInit {
-  @Input() control: FormControl;
-  constructor() {}
+export class TextareaComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder = '';
+  value: string;
+  onChange: (val) => void;
+  onTouched: () => {};
+  disabled: boolean;
 
+  constructor() {}
   ngOnInit(): void {}
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(value: string): void {
+    this.value = value ? value : '';
+  }
 }
