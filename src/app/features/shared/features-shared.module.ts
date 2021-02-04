8b1bc1bbafb423cf7604362ca05a1b5f8b42c01a
Gvrito123 <gvritagio@gmail.com>
2021-02-04 20:11:58 +0400

    feat(bank-trans-details) imported SharedModule for tags to work

--- src/app/features/shared/features-shared.module.ts
+++ src/app/features/shared/features-shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { components } from './components';
+import { SharedModule } from '@shared/shared';
 
 @NgModule({
   declarations: [...components],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [...components],
   providers: [],
 })
