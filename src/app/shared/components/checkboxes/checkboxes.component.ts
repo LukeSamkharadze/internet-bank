e2060cf5f27f16b161d351afe47297853245601d
user1 <62304291+Qrisno@users.noreply.github.com>
2021-02-04 22:34:46 +0400

    added custom form controls

--- src/app/shared/components/checkboxes/checkboxes.component.ts
+++ src/app/shared/components/checkboxes/checkboxes.component.ts
@@ -1,16 +1,74 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ElementRef,
+  Self,
+  ViewChild,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NgControl,
+  NG_VALUE_ACCESSOR,
+  NG_VALIDATORS,
+  Validator,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-shared-checkboxes',
   templateUrl: './checkboxes.component.html',
   styleUrls: ['./checkboxes.component.scss'],
 })
-export class CheckboxesComponent implements OnInit {
+export class CheckboxesComponent
+  implements ControlValueAccessor, Validator, OnInit {
+  @ViewChild('input') input: ElementRef;
   @Input() checkboxType = 'uncheck';
+  @Input() isRequired = false;
   @Input() checked = false;
   @Input() disabled = false;
   @Input() checkboxError = '';
-  constructor() {}
+  defaultValue: any;
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    const control = this.controlDir.control;
+    const validators: ValidatorFn[] = control.validator
+      ? [control.validator]
+      : [];
+    if (this.isRequired) {
+      validators.push(Validators.required);
+    }
+    control.setValidators(validators);
+    control.updateValueAndValidity();
+  }
+
+  onChange(event) {}
+
+  onTouched() {}
+
+  writeValue(obj: any): void {}
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  validate(c: AbstractControl): { [key: string]: any } {
+    const validators: ValidatorFn[] = [];
+    if (this.isRequired) {
+      validators.push(Validators.required);
+    }
+    return validators;
+  }
 }
