b58565541b4d48cb042e4e0c7be8ac48df6786bf
Luka Samkharadze <samkharadzeluke@gmail.com>
2021-02-04 13:01:26 +0400

    feat(dropdown): add ability to select option

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  AfterContentInit,
+  ContentChild,
+  Directive,
+} from '@angular/core';
 
 @Component({
   selector: 'app-shared-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
 })
-export class DropdownComponent implements OnInit {
-  constructor() {}
+export class DropdownComponent implements OnInit, AfterViewInit {
+  selectedRef: Element;
 
-  @Input() formControl: FormControl;
-  @Input() optionsCount = 0;
+  constructor(private thisComponent: ElementRef) {}
 
   isOptionsOpened = true;
+
+  ngAfterViewInit() {
+    this.selectedRef = (this.thisComponent
+      .nativeElement as Element).getElementsByClassName('content')[0];
+
+    (this.thisComponent.nativeElement as Element)
+      .querySelectorAll('.option')
+      .forEach((o) =>
+        o.addEventListener('click', () => {
+          this.selectedRef.innerHTML = o.outerHTML;
+        })
+      );
+  }
 
   ngOnInit(): void {}
 }
