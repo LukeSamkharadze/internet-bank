669e1fda2678b402f5d45d95a98985a34c0a37ed
mariam-ts <tsirekidze.ma@gmail.com>
2021-02-06 00:02:57 +0400

    fixed

--- src/app/shared/components/toggle/toggle.component.ts
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -1,12 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import {
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-toggle',
+  selector: 'app-shared-toggle',
   templateUrl: './toggle.component.html',
   styleUrls: ['./toggle.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => ToggleComponent),
+    },
+  ],
 })
-export class ToggleComponent implements OnInit {
-  constructor() {}
+export class ToggleComponent implements OnInit, ControlValueAccessor {
+  @Input() toggleType: string;
+  public toggle = new FormControl('');
+  private onChange: () => void;
+  private onTouched: () => void;
 
-  ngOnInit(): void {}
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  writeValue(obj: any): void {
+    console.log(obj);
+    this.toggle.setValue(obj);
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.toggle.disable();
+    } else {
+      this.toggle.enable();
+    }
+  }
+
+  toggleisDisabled(): boolean {
+    if (this.toggleType === 'on-disable' || this.toggleType === 'off-disable') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.setDisabledState(this.toggleisDisabled());
+  }
 }
