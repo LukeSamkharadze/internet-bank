5a6464afa29376619043b189b20eb5ca1ec04cbb
Guro <gurammateshvili3@gmail.com>
2021-02-05 11:43:47 +0400

    fix: content-main height issue and add component to index.ts

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,9 +1,16 @@
 import { InputComponent } from './input/input.component';
 import { TextareaComponent } from './textarea/textarea.component';
 import { TagComponent } from './tag/tag.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export * from './input/input.component';
 export * from './textarea/textarea.component';
 export * from './tag/tag.component';
+export * from './layout/layout.component';
 
-export const components = [InputComponent, TextareaComponent, TagComponent];
+export const components = [
+  InputComponent,
+  TextareaComponent,
+  TagComponent,
+  LayoutComponent,
+];
