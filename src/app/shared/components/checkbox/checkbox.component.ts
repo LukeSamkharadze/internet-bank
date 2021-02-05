e49a25484ae4a9db427caa4b9e48490fb301e83e
user1 <62304291+Qrisno@users.noreply.github.com>
2021-02-05 14:26:45 +0400

    added ngmodel

--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,27 +1,25 @@
 import {
   ElementRef,
-  Self,
   ViewChild,
   Component,
   Input,
-  OnInit,
+  forwardRef,
 } from '@angular/core';
-import {
-  AbstractControl,
-  ControlValueAccessor,
-  NgControl,
-  Validator,
-  ValidatorFn,
-  Validators,
-} from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-shared-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true,
+    },
+  ],
 })
-export class CheckboxComponent
-  implements ControlValueAccessor, Validator, OnInit {
+export class CheckboxComponent implements ControlValueAccessor {
   @ViewChild('input') input: ElementRef;
   @Input() textBefore = false;
   @Input() checkboxId = 'uncheck';
@@ -29,44 +27,30 @@ export class CheckboxComponent
   @Input() checked = false;
   @Input() disabled = false;
   @Input() checkboxError = '';
-  defaultValue: any;
-  constructor(@Self() public controlDir: NgControl) {
-    this.controlDir.valueAccessor = this;
+
+  isChecked = false;
+  onChange = (_) => {};
+  onBlur = (_) => {};
+
+  writeValue(obj: boolean): void {
+    this.isChecked = obj;
   }
-
-  ngOnInit() {
-    const control = this.controlDir.control;
-    const validators: ValidatorFn[] = control.validator
-      ? [control.validator]
-      : [];
-    if (this.isRequired) {
-      validators.push(Validators.required);
-    }
-    control.setValidators(validators);
-    control.updateValueAndValidity();
-  }
-
-  onChange(event) {}
-
-  onTouched = () => {};
-
-  writeValue(obj: any): void {}
 
   registerOnChange(fn: any): void {
+    console.log(this.onChange);
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any): void {
-    this.onTouched = fn;
+    this.onBlur = fn;
   }
+
   setDisabledState?(isDisabled: boolean): void {
     this.disabled = isDisabled;
   }
 
-  validate(c: AbstractControl): { [key: string]: any } {
-    const validators: ValidatorFn[] = [];
-    if (this.isRequired) {
-      validators.push(Validators.required);
-    }
-    return validators;
+  onChanged($event) {
+    this.isChecked = $event && $event.target && $event.target.checked;
+    this.onChange(this.isChecked);
   }
 }
