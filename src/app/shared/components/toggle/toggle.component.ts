add8994fe549b751e74f5ea0f908321e36348941
mariam-ts <tsirekidze.ma@gmail.com>
2021-02-07 22:01:22 +0400

    fixed

--- src/app/shared/components/toggle/toggle.component.ts
+++ src/app/shared/components/toggle/toggle.component.ts
@@ -1,5 +1,11 @@
 import { Component, forwardRef, Input, OnInit } from '@angular/core';
-import { FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-shared-toggle',
@@ -11,65 +17,80 @@ import { FormControl, NG_VALUE_ACCESSOR, Validators } from '@angular/forms';
       multi: true,
       useExisting: forwardRef(() => ToggleComponent),
     },
+    { provide: NG_VALIDATORS, useExisting: ToggleComponent, multi: true },
   ],
 })
-export class ToggleComponent implements OnInit {
-  @Input() toggleType: string;
-  @Input() checked: boolean | string;
+export class ToggleComponent implements OnInit, ControlValueAccessor {
+  @Input() textBefore = false;
+  @Input() checked = false;
+  @Input() disabled = false;
+  @Input() checkboxError = false;
   @Input() required: boolean | string;
+  @Input() toggleType;
+  @Input() toggleId;
+  isChecked = false;
+  control: AbstractControl;
+  public toggleClass;
+  onChange = (_) => {};
+  onBlur = (_) => {};
 
-  public toggle;
-  private onChange: () => void;
-  private onTouched: () => void;
-
-  validate(): void {
+  validate(control: AbstractControl): { [key: string]: any } | null {
     if (this.required || this.required === '') {
-      this.toggle = new FormControl('', [Validators.requiredTrue]);
-    } else {
-      this.toggle = new FormControl();
+      this.control = control;
+      return control.value + '' !== 'true'
+        ? {
+            appRequiredTrue: {
+              message: 'Field required',
+              value: control.value,
+            },
+          }
+        : null;
     }
-    if (this.checked === '') {
-      this.checked = true;
+
+    return null;
+  }
+
+  checkDisabled() {
+    if (!this.disabled) {
+      this.checked = !this.checked;
+      this.onChange(this.checked);
+      this.control.markAsTouched();
     }
   }
+  writeValue(obj: boolean): void {
+    this.isChecked = obj;
+  }
+
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-  writeValue(obj: any): void {
-    console.log(obj);
-    this.toggle.setValue(obj);
-  }
-  setDisabledState?(isDisabled: boolean): void {
-    if (isDisabled) {
-      this.toggle.disable();
-    } else {
-      this.toggle.enable();
-    }
-  }
-  doBlur() {
-    this.onTouched();
+    this.onBlur = fn;
   }
 
-  toggleisDisabled(): boolean {
-    if (
-      this.toggleType === 'off-disabled' ||
-      this.toggleType === 'on-disabled'
-    ) {
-      this.toggleType === 'off-disabled'
-        ? this.toggle.setValue(false)
-        : this.toggle.setValue(true);
-      return true;
-    } else {
-      return false;
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onChanged($event) {
+    this.isChecked = $event && $event.target && $event.target.checked;
+    this.onChange(this.isChecked);
+  }
+
+  setToggleClass() {
+    if (this.disabled !== false && this.checked) {
+      this.toggleClass = 'on-disabled';
+    } else if (this.disabled !== false && !this.checked) {
+      this.toggleClass = 'off-disabled';
+    } else if (this.toggleType === 'success') {
+      this.toggleClass = 'success-on';
+    } else if (this.toggleType === 'error') {
+      this.toggleClass = 'error-off';
     }
   }
 
   ngOnInit(): void {
-    this.validate();
-    this.toggle.setValue(this.checked);
-    this.setDisabledState(this.toggleisDisabled());
+    this.setToggleClass();
   }
 }
