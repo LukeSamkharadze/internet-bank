42682a1fb508f4ea0ecefaa5f9e194206db919da
user2 <aslanioniani2000@gmail.com>
2021-02-08 20:32:24 +0400

    (feat-notifications): adding click methods

--- src/app/features/shared/components/feat-notification/feat-notification.component.ts
+++ src/app/features/shared/components/feat-notification/feat-notification.component.ts
@@ -7,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeatNotificationComponent implements OnInit {
   successfulPay: boolean = true;
+  unsuccessfulPay: boolean = true;
+  bellNotifications: boolean = false;
+
   constructor() {}
 
   successfulPayMethod() {
+    this.successfulPay = false;
     return this.successfulPay;
+  }
+
+  unsuccessfulPayMethod() {
+    this.unsuccessfulPay = false;
+    return this.unsuccessfulPay;
+  }
+
+  bellMethod() {
+    this.bellNotifications = !this.bellNotifications;
+    return this.bellNotifications;
   }
   ngOnInit(): void {}
 }
