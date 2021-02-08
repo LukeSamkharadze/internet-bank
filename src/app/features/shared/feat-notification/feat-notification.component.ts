2ae09fb0e320c2050d9f09d3ea92854b4fdb2e31
user2 <aslanioniani2000@gmail.com>
2021-02-08 19:21:14 +0400

    feat-notifications: adding components

--- src/app/features/shared/feat-notification/feat-notification.component.ts
+++ src/app/features/shared/feat-notification/feat-notification.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feat-notification.component.scss'],
 })
 export class FeatNotificationComponent implements OnInit {
+  successfulPay: boolean = true;
   constructor() {}
 
+  successfulPayMethod() {
+    return this.successfulPay;
+  }
   ngOnInit(): void {}
 }
