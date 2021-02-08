d79ad596fb14bc88faeff0c27e9a69b1418a82cb
Luka Samkharadze <samkharadzeluke@gmail.com>
2021-02-09 02:04:55 +0400

    feat(dropdown): add custom forms logic

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,39 +1,60 @@
+import { Component, Input, TemplateRef, forwardRef } from '@angular/core';
 import {
-  Component,
-  OnInit,
-  Input,
-  ViewChild,
-  AfterViewInit,
-  ElementRef,
-  AfterContentInit,
-  ContentChild,
-  Directive,
-} from '@angular/core';
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  SelectControlValueAccessor,
+} from '@angular/forms';
+import { FeaturesSharedModule } from '@features/shared';
 
 @Component({
   selector: 'app-shared-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DropdownComponent),
+      multi: true,
+    },
+  ],
 })
-export class DropdownComponent implements OnInit, AfterViewInit {
-  selectedRef: Element;
+export class DropdownComponent implements ControlValueAccessor {
+  @Input() value: any;
+  @Input() placeholder: any;
+  @Input() options: any = [];
 
-  constructor(private thisComponent: ElementRef) {}
+  @Input() placeholderTemplate: TemplateRef<any>;
+  @Input() selectedTemplate: TemplateRef<any>;
+  @Input() optionTemplate: TemplateRef<any>;
 
-  isOptionsOpened = true;
+  isOptionsOpened = false;
+  isPlaceholderOn = true;
+  disabled = false;
 
-  ngAfterViewInit() {
-    this.selectedRef = (this.thisComponent
-      .nativeElement as Element).getElementsByClassName('content')[0];
+  onChange: (_: any) => void;
+  onTouched: () => void;
 
-    (this.thisComponent.nativeElement as Element)
-      .querySelectorAll('.option')
-      .forEach((o) =>
-        o.addEventListener('click', () => {
-          this.selectedRef.innerHTML = o.outerHTML;
-        })
-      );
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: (value: any) => any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    console.log(isDisabled);
+    // throw new Error('Method not implemented.');
   }
 
-  ngOnInit(): void {}
+  dropdownClicked() {
+    this.isOptionsOpened = !this.isOptionsOpened;
+  }
+
+  optionClicked(option: any) {
+    this.isPlaceholderOn = false;
+    this.value = option;
+    this.onChange(option);
+  }
 }
