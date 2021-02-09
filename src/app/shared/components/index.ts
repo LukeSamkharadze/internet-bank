3e1b7c05d9aaa7585c16dbae1ea09f3782af9ba5
Teona-tech <teoforchxidze30@gmail.com>
2021-02-09 19:54:06 +0400

    fixed notifications

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -21,8 +21,8 @@ export const components = [
   TextareaComponent,
   TagComponent,
   ButtonComponent,
-  NotificationComponent,
   ProgressBarsComponent,
   CheckboxComponent,
   LayoutComponent,
+  NotificationComponent,
 ];
