48d3a1ad8111968735b6217039489f3b1a138fe7
user2 <oto.chechelashvili@gmail.com>
2021-02-10 13:26:25 +0400

    security settings visual part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { SettingsSecurityComponent } from './features/settings-security/settings-security.component';
+import{ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule],
+  declarations: [AppComponent, SettingsSecurityComponent],
+  imports: [BrowserModule, CoreModule, AppRoutingModule,SharedModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
