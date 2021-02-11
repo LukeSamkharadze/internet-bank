81a3ee705cdf2bddae5dfb7324fa82e12c2163c6
nertc <davidtsiklauri7@gmail.com>
2021-02-11 11:06:36 +0400

    feat(feat/accounts-list charts): Finish charts http service

--- src/app/features/accounts-list/accounts-list.component.ts
+++ src/app/features/accounts-list/accounts-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IncomeDataType } from '../shared/components/income-chart/services/data/dataType';
+import { IncomeDataService } from '../shared/components/income-chart/services/data/income-data.service';
 import { ICard } from '../shared/interfaces/card.interface';
 import { IDeposit } from '../shared/interfaces/deposit.interface';
 import { ILoan } from '../shared/interfaces/loan.interface';
-import AccountType from './models/account-type.enum';
-import IItem from './models/list-item.entity';
 import { AccountsListInfoService } from './services/accounts-list-info.service';
 
 @Component({
@@ -15,12 +15,17 @@ export class AccountsListComponent implements OnInit {
   cards: Array<ICard> = [];
   deposits: Array<IDeposit> = [];
   loans: Array<ILoan> = [];
+  incomes: Array<IncomeDataType> = [];
 
-  constructor(public infoService: AccountsListInfoService) {}
+  constructor(
+    public infoService: AccountsListInfoService,
+    private incomeService: IncomeDataService
+  ) {}
 
   ngOnInit(): void {
     this.infoService.getCards().subscribe((v) => (this.cards = v));
     this.infoService.getDeposits().subscribe((v) => (this.deposits = v));
     this.infoService.getLoans().subscribe((v) => (this.loans = v));
+    this.incomeService.getAll().subscribe((v) => (this.incomes = v));
   }
 }
--- src/app/features/shared/components/income-chart/services/data/dataType.ts
+++ src/app/features/shared/components/income-chart/services/data/dataType.ts
@@ -1,4 +1,5 @@
 export interface IncomeDataType {
   id: number;
   data: number[];
+  name?: string;
 }
