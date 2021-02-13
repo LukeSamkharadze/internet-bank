9edc1743ed927ffcd8741d6e42671793a9deb731
Nmiquchadze <Naniko@DESKTOP-P4I4BTD>
2021-02-14 02:48:44 +0400

    fix (bank-transactions) angular #5 minor changes

--- src/app/features/shared/features-shared.module.ts
+++ src/app/features/shared/features-shared.module.ts
@@ -5,11 +5,19 @@ import { components } from './components';
 import { SharedModule } from '@shared/shared';
 import { CardService } from './services/card.service';
 
-const services = [CardService];
-const modules = [CommonModule, SharedModule];
-
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+const services = [CardService];
+const modules = [
+  CommonModule,
+  SharedModule,
+  Ng2SearchPipeModule,
+  FormsModule,
+  BrowserModule,
+];
 
 @NgModule({
   declarations: [...components],
