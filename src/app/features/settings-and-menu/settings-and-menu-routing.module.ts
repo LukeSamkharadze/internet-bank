4954e249e5306233af32bb871ed5941e52a5f954
javaxenco <datojavaxishvili15@gmail.com>
2021-02-15 23:09:50 +0400

    feat(general-settings-menu)Routes Left

--- src/app/features/settings-and-menu/settings-and-menu-routing.module.ts
+++ src/app/features/settings-and-menu/settings-and-menu-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsAndMenuComponent } from './settings-and-menu.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   {
