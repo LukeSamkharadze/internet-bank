2282fb093fe0b139cd5f0cfafc7034dd3a7935b9
AnaMaghradze <annmaghradze@gmail.com>
2021-02-20 00:04:04 +0400

    fix(feature/news-list): fixed news-list width

--- src/app/features/news/services/news.service.ts
+++ src/app/features/news/services/news.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { scan } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
