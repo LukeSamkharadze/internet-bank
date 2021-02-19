70e2fefce0bcac04fd4139fb99ecffa9c63ad22c
AnaMaghradze <annmaghradze@gmail.com>
2021-02-20 03:09:04 +0400

    fix(feature/news-list): added routing module and fixed styles

--- src/app/features/news/news.module.ts
+++ src/app/features/news/news.module.ts
@@ -5,10 +5,11 @@ import { NewsItemComponent } from './news-item/news-item.component';
 import { FeaturesSharedModule } from '../shared/features-shared.module';
 import { NewsListComponent } from './news-list/news-list.component';
 import { NewsService } from './services/news.service';
+import { NewsRoutingModule } from './news-routing.module';
 
 @NgModule({
   declarations: [NewsComponent, NewsItemComponent, NewsListComponent],
-  imports: [SharedModule, FeaturesSharedModule],
+  imports: [SharedModule, FeaturesSharedModule, NewsRoutingModule],
   providers: [NewsService],
   exports: [NewsComponent],
 })
