1d99b5058ccd5efe25c3ef3a63334aa9b79f82ba
tschikovani <tsotnechikovani@gmail.com>
2021-02-19 19:48:59 +0400

    feat - add cardType type

--- src/app/features/shared/interfaces/card.interface.ts
+++ src/app/features/shared/interfaces/card.interface.ts
@@ -2,9 +2,13 @@ import IParent from './parent.interface';
 
 export interface ICard extends IParent {
   cardName: string;
+  cardType: CardType;
   cardNumber: string;
   cardholder: string;
   availableAmount: number;
   security3D: boolean;
   iconPath?: string;
 }
+
+
+export type CardType = 'VISA' | 'MASTERCARD';
