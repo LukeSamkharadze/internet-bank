fdf2584ecc198629128a0afb0f4f9831d9024e8a
Ezab <ekzabakhidze@gmail.com>
2021-02-25 02:58:50 +0400

    feat(accounts spendings): fix interface path

--- src/app/features/shared/components/accounts-spendings/accounts-spendings.component.ts
+++ src/app/features/shared/components/accounts-spendings/accounts-spendings.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Spendings } from './spendings.model';
+import { Spendings } from '../../interfaces/spendings.interface';
 
 @Component({
   selector: 'app-shared-accounts-spendings',
