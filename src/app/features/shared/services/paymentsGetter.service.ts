ca7712e593caddf78e1e0b5b9ad6db6f8a4789bc
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-02-25 19:49:51 +0400

    fix(feat/payments): commented out payments getter code & will delete later

--- src/app/features/shared/services/paymentsGetter.service.ts
+++ src/app/features/shared/services/paymentsGetter.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BankTransfer } from '../interfaces/bankPayment.entity';
-import { InstantTransfer } from '../interfaces/instantPaymententity';
-import { ElectronicTransfer } from '../interfaces/electronicPayment.entity';
+import { BankPayment } from '../interfaces/bankPayment.entity';
+import { InstantPayment } from '../interfaces/instantPaymententity';
+import { ElectronicPayment } from '../interfaces/electronicPayment.entity';
 import { environment } from '../../../../environments/environment';
 import { Observable, forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -13,42 +13,40 @@ import { map } from 'rxjs/operators';
 export class PaymentsGetterService {
   constructor(private http: HttpClient) {}
 
-  getById(
-    paymentId: number
-  ): Observable<BankTransfer[] | ElectronicTransfer[] | InstantTransfer[]> {
-    return this.http.get<
-      BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
-    >(environment.BaseUrl + `payments?id=${paymentId}`);
+  getById(paymentId: number) {
+    // return this.http.get<
+    //   BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
+    // >(environment.BaseUrl + `payments?id=${paymentId}`);
   }
 
   getByAccountNumber(accountNumber: string) {
-    const expenses = this.http.get<
-      BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
-    >(environment.BaseUrl + `payments?fromAccount=${accountNumber}`);
-
-    const income = this.http.get<
-      BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
-    >(
-      environment.BaseUrl + `payments?destinationAccountNumber=${accountNumber}`
-    );
-
-    return forkJoin([expenses, income]).pipe(
-      map((results) => [...results[0], ...results[1]])
-    );
+    // const expenses = this.http.get<
+    //   BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
+    // >(environment.BaseUrl + `payments?fromAccount=${accountNumber}`);
+    //
+    // const income = this.http.get<
+    //   BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
+    // >(
+    //   environment.BaseUrl + `payments?destinationAccountNumber=${accountNumber}`
+    // );
+    //
+    // return forkJoin([expenses, income]).pipe(
+    //   map((results) => [...results[0], ...results[1]])
+    // );
   }
 
   getByUserId(userId: string) {
-    const expenses = this.http.get<
-      BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
-    >(environment.BaseUrl + `payments?fromUserId=${userId}`);
-
-    const income = this.http.get<
-      BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
-    >(environment.BaseUrl + `payments?destinationAccountUserId=${userId}`);
-
-    return forkJoin([expenses, income]).pipe(
-      map((results) => [...results[0], ...results[1]])
-    );
+    // const expenses = this.http.get<
+    //   BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
+    // >(environment.BaseUrl + `payments?fromUserId=${userId}`);
+    //
+    // const income = this.http.get<
+    //   BankTransfer[] | InstantTransfer[] | ElectronicTransfer[]
+    // >(environment.BaseUrl + `payments?destinationAccountUserId=${userId}`);
+    //
+    // return forkJoin([expenses, income]).pipe(
+    //   map((results) => [...results[0], ...results[1]])
+    // );
   }
 
   // Feel free to add getter by another specific property of payment.
