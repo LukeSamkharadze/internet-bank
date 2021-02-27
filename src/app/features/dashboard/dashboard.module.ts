061a527c02467f5f1049ce688fd820406de0f9f3
Luka Samkharadze <samkharadzeluke@gmail.com>
2021-02-27 23:48:14 +0400

    feat(dashboard): lazy load

---
+++ src/app/features/dashboard/dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard.component';
+import { FeaturesSharedModule } from '@features/shared';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    FeaturesSharedModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
