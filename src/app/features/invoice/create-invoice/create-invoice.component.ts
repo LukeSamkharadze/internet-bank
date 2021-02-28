dc745cb6881a30d6e9c55f73a746144b47b0d1d2
mariam-ts <tsirekidze.ma@gmail.com>
2021-02-28 23:58:41 +0400

    axla martla movrchi

--- src/app/features/invoice/create-invoice/create-invoice.component.ts
+++ src/app/features/invoice/create-invoice/create-invoice.component.ts
@@ -1,13 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-invoice',
   templateUrl: './create-invoice.component.html',
   styleUrls: ['./create-invoice.component.scss'],
 })
-export class CreateInvoiceComponent implements OnInit {
-  @Input() routing;
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class CreateInvoiceComponent {}
