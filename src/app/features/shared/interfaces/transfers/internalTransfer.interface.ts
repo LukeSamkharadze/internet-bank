2871bd863e0881a60e3d58e708d15da89f85aefb
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-03-02 00:17:57 +0400

    fix(feat/payments): renamed leftover instant payments & removed limits check from internal

--- src/app/features/shared/interfaces/transfers/internalTransfer.interface.ts
+++ src/app/features/shared/interfaces/transfers/internalTransfer.interface.ts
@@ -1,6 +1,6 @@
 import { Transfer } from './transfer.interface';
 
 export interface InternalTransfer extends Transfer {
-  instantTransferType: string;
+  internalTransferType: string;
   toAccountNumber: string;
 }
