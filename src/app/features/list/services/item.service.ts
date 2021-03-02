f6415be87569b43fecb5ab6de6e8570f7e270291
Vitruvius21 <38228361+Vitruvius21@users.noreply.github.com>
2021-03-02 18:05:24 +0400

    fix(general): clean-up unused operators

--- src/app/features/list/services/item.service.ts
+++ src/app/features/list/services/item.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { ItemEntity } from '../models/item.entity';
-import { delay, filter, switchMap, tap, toArray } from 'rxjs/operators';
+import { filter, switchMap, toArray } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
@@ -10,7 +10,6 @@ export class ItemService {
 
   getItems(text: string = ''): Observable<ItemEntity[]> {
     return this.http.get<ItemEntity[]>('http://localhost:3000/items').pipe(
-      // tap(value => console.log(value)),
       switchMap((data) => from(data)),
       filter((data) => data.name.includes(text)),
       toArray()
