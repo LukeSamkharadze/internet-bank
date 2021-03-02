61e32cd03dc767e204002e29eb4f2681852bd76f
Vitruvius21 <38228361+Vitruvius21@users.noreply.github.com>
2021-03-02 17:31:57 +0400

    fix(create-card): revorke typeSwitch service

--- src/app/features/create-card/services/type-switcher.service.ts
+++ src/app/features/create-card/services/type-switcher.service.ts
@@ -1,40 +1,54 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { BackgroundService } from '../../shared/services/background.service';
+import { CardService } from '../../shared/services/card.service';
+import { IconService } from '../../shared/services/icon.service';
+import { ICard, CardType } from '../../shared/interfaces/card.interface';
 
 @Injectable()
 export class TypeSwitcherService {
   cardType$: BehaviorSubject<string> = new BehaviorSubject('');
   cardIconUrl$: BehaviorSubject<string> = new BehaviorSubject('');
   cardBgUrl$: BehaviorSubject<string> = new BehaviorSubject(
-    './assets/cards/backgrounds/light-orange.svg'
+    this.backgroundService.getBackground('light-orange')
   );
   color$: BehaviorSubject<string> = new BehaviorSubject('');
 
-  constructor() {}
+  constructor(
+    private backgroundService: BackgroundService,
+    private cardService: CardService,
+    private iconService: IconService
+  ) {}
 
-  checkInput(input?: string) {
-    switch (input) {
-      case '4':
-        return (
-          this.cardType$.next('Visa card'),
-          this.cardIconUrl$.next('./assets/cards/visa.svg'),
-          this.cardBgUrl$.next('./assets/cards/backgrounds/blue.svg'),
-          this.color$.next('visa-cl')
-        );
-      case '5':
-        return (
-          this.cardType$.next('Mastercard'),
-          this.cardIconUrl$.next('./assets/cards/mastercard.svg'),
-          this.cardBgUrl$.next('./assets/cards/backgrounds/orange.svg'),
-          this.color$.next('master-cl')
-        );
-      default:
-        return (
-          this.cardType$.next(''),
-          this.cardIconUrl$.next(''),
-          this.cardBgUrl$.next('./assets/cards/backgrounds/light-orange.svg'),
-          this.color$.next('')
-        );
+  checkInput(inCard: ICard) {
+    const card = this.cardService.determineCardType(inCard);
+    if (card.cardType) {
+      const cardType = this.transformCardType(card.cardType);
+      // Visa or Mastercard values
+      this.cardType$.next(cardType);
+      this.cardIconUrl$.next(this.iconService.determineCardIcon(card).iconPath),
+        this.cardBgUrl$.next(this.cardService.determineBackground(card)),
+        this.color$.next(this.cardService.determineColor(card));
+    } else {
+      // Default value
+      this.assignDefault();
     }
+  }
+
+  assignDefault() {
+    this.cardType$.next(''),
+      this.cardIconUrl$.next(''),
+      this.cardBgUrl$.next(
+        this.backgroundService.getBackground('light-orange')
+      ),
+      this.color$.next('');
+  }
+
+  // Uppercase 'cardType' first letter, if 'visa' add " card" string at the end
+  transformCardType(cardType: CardType) {
+    if (cardType === 'visa') {
+      return cardType.charAt(0).toUpperCase() + cardType.substring(1) + ' card';
+    }
+    return cardType.charAt(0).toUpperCase() + cardType.substring(1);
   }
 }
