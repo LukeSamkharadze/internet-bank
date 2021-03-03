907709a4eefe25612719d857d6d494aeda578ae0
nertc <davidtsiklauri7@gmail.com>
2021-03-03 20:43:12 +0400

    fix(feat/accounts-list charts): Fix disappearing when no item

--- src/app/features/accounts-list/accounts-list.component.ts
+++ src/app/features/accounts-list/accounts-list.component.ts
@@ -1,19 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { combineLatest, from, Observable, of } from 'rxjs';
-import {
-  concatMap,
-  first,
-  map,
-  mergeMap,
-  scan,
-  switchMap,
-} from 'rxjs/operators';
+import { combineLatest, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ICard } from '../shared/interfaces/card.interface';
 import { IDeposit } from '../shared/interfaces/deposit.interface';
 import { ILoan } from '../shared/interfaces/loan.interface';
-import { FormatterService } from '../shared/services/formatter.service';
 import IItem from './models/chart-item.interface';
-import IChartItem from './models/chart-server-item.interface';
 import { AccountsListIncomeService } from './services/accounts-list-income.service';
 import { AccountsListInfoService } from './services/accounts-list-info.service';
 
@@ -28,11 +19,13 @@ export class AccountsListComponent implements OnInit {
   deposits$: Observable<Array<IDeposit>>;
   loans$: Observable<Array<ILoan>>;
   incomes$: Observable<Observable<IItem>[]>;
+  incomeColumns$: Observable<number>;
+
+  private readonly MAX_COLUMNS_CHARTS = 3;
 
   constructor(
     public infoService: AccountsListInfoService,
-    private incomeService: AccountsListIncomeService,
-    private formatService: FormatterService
+    private incomeService: AccountsListIncomeService
   ) {}
 
   ngOnInit(): void {
@@ -41,77 +34,14 @@ export class AccountsListComponent implements OnInit {
     this.loans$ = this.infoService.getLoans();
 
     this.incomes$ = combineLatest([
-      this.generateCardsChart(this.cards$),
-      this.generateDepositChart(this.deposits$),
-      this.generateLoansChart(this.loans$),
-    ]).pipe(map((val) => val.map((v) => of(v))));
-  }
+      this.incomeService.generateCardsChart(this.cards$),
+      this.incomeService.generateDepositChart(this.deposits$),
+      this.incomeService.generateLoansChart(this.loans$),
+    ]).pipe(map((val) => val.filter((v) => v).map((v) => of(v))));
 
-  generateCardsChart(cards$: Observable<ICard[]>): Observable<IItem> {
-    const cardsBalance$ = cards$.pipe(
-      switchMap((cards) => from(cards)),
-      map((card) => card.balance || card.availableAmount || 0),
-      scan((acc, cur) => acc + cur, 0)
+    this.incomeColumns$ = this.incomes$.pipe(
+      map((incomes) => incomes.length),
+      map((length) => Math.min(length, this.MAX_COLUMNS_CHARTS))
     );
-    const cardsChart$ = combineLatest([
-      cardsBalance$,
-      this.incomeService.getForUser('card'),
-    ]).pipe(
-      map(
-        ([balance, chart]) =>
-          ({
-            title: 'Cards balance',
-            value: balance,
-            data: chart.data,
-          } as IItem)
-      )
-    );
-    return cardsChart$;
-  }
-
-  generateDepositChart(deposits$: Observable<IDeposit[]>): Observable<IItem> {
-    const deposit$ = deposits$.pipe(
-      switchMap((deposits) => deposits),
-      first()
-    );
-    const depositChart$ = combineLatest([
-      deposit$,
-      this.incomeService.getForUser('deposit'),
-    ]).pipe(
-      map(
-        ([deposit, chart]) =>
-          ({
-            title: `Deposit (${this.formatService.formatBalance(
-              Math.floor(deposit.depositRate * 100),
-              { currency: '%', toRight: true }
-            )} Rate)`,
-            value: deposit.balance,
-            data: chart.data,
-          } as IItem)
-      )
-    );
-    return depositChart$;
-  }
-
-  generateLoansChart(loans$: Observable<ILoan[]>): Observable<IItem> {
-    const loansBalance$ = loans$.pipe(
-      switchMap((loans) => loans),
-      map((loan) => (loan.balance || loan.paid || 0) - (loan.paid || 0)),
-      scan((acc, cur) => acc + cur, 0)
-    );
-    const loansChart$ = combineLatest([
-      loansBalance$,
-      this.incomeService.getForUser('loan'),
-    ]).pipe(
-      map(
-        ([balance, chart]) =>
-          ({
-            title: 'Pending credit',
-            value: balance,
-            data: chart.data,
-          } as IItem)
-      )
-    );
-    return loansChart$;
   }
 }
--- src/app/features/accounts-list/services/accounts-list-income.service.ts
+++ src/app/features/accounts-list/services/accounts-list-income.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { first, retry, switchMap } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { first, map, retry, switchMap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { ICard } from '../../shared/interfaces/card.interface';
+import { IDeposit } from '../../shared/interfaces/deposit.interface';
+import { ILoan } from '../../shared/interfaces/loan.interface';
 import { AuthService } from '../../shared/services/auth.service';
+import { FormatterService } from '../../shared/services/formatter.service';
+import IItem from '../models/chart-item.interface';
 import IChartItem, { IChartType } from '../models/chart-server-item.interface';
 
 @Injectable()
@@ -14,7 +19,11 @@ export class AccountsListIncomeService {
     ['loan', 'loanCharts'],
   ]);
 
-  constructor(private http: HttpClient, private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private formatService: FormatterService
+  ) {}
 
   getForUser(type: IChartType): Observable<IChartItem> {
     return this.http
@@ -30,9 +39,81 @@ export class AccountsListIncomeService {
       );
   }
 
-  bias(data: number[], amount: number): number[] {
-    const bias = amount / data[data.length - 1];
-    data = data.map((num) => num * bias);
-    return data;
+  generateCardsChart(cards$: Observable<ICard[]>): Observable<IItem | null> {
+    const cardsBalance$ = cards$.pipe(
+      map((cards) =>
+        cards.map((card) => card.balance || card.availableAmount || 0)
+      ),
+      map((cards) =>
+        cards.length ? cards.reduce((acc, cur) => acc + cur, 0) : null
+      )
+    );
+    const cardsChart$ = combineLatest([
+      cardsBalance$,
+      this.getForUser('card'),
+    ]).pipe(
+      map(([balance, chart]) =>
+        balance
+          ? ({
+              title: 'Cards balance',
+              value: balance,
+              data: chart.data,
+            } as IItem)
+          : null
+      )
+    );
+    return cardsChart$;
+  }
+
+  generateDepositChart(
+    deposits$: Observable<IDeposit[]>
+  ): Observable<IItem | null> {
+    const deposit$ = deposits$.pipe(
+      map((deposits) => (deposits.length ? deposits[0] : null))
+    );
+    const depositChart$ = combineLatest([
+      deposit$,
+      this.getForUser('deposit'),
+    ]).pipe(
+      map(([deposit, chart]) =>
+        deposit
+          ? ({
+              title: `Deposit (${this.formatService.formatBalance(
+                Math.floor(deposit.depositRate * 100),
+                { currency: '%', toRight: true }
+              )} Rate)`,
+              value: deposit.balance,
+              data: chart.data,
+            } as IItem)
+          : null
+      )
+    );
+    return depositChart$;
+  }
+
+  generateLoansChart(loans$: Observable<ILoan[]>): Observable<IItem | null> {
+    const loansBalance$ = loans$.pipe(
+      map((loans) =>
+        loans.map((loan) => (loan.balance || loan.paid || 0) - (loan.paid || 0))
+      ),
+      map((loans) =>
+        loans.length ? loans.reduce((acc, cur) => acc + cur, 0) : null
+      )
+    );
+    const loansChart$ = combineLatest([
+      loansBalance$,
+      this.getForUser('loan'),
+    ]).pipe(
+      map(([balance, chart]) =>
+        balance
+          ? ({
+              title: 'Pending credit',
+              value: balance,
+              data: chart.data,
+            } as IItem)
+          : null
+      )
+    );
+    return loansChart$;
   }
 }
