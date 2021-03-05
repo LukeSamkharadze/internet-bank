deb0bee8fe6f6dfea433fcc8683068cd5c3c05c2
vazha21 <vazha.abdushelishvili.1@btu.edu.ge>
2021-03-06 00:00:33 +0400

    feat(sockets): update edit user  form when editing user profile

--- src/app/features/settings-and-menu/settings/settings.component.ts
+++ src/app/features/settings-and-menu/settings/settings.component.ts
@@ -6,12 +6,13 @@ import {
   FormControl,
 } from '@angular/forms';
 
-import { Observable, Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 import { AuthService } from '../../shared/services/auth.service';
 import { UserService } from '../../shared/services/user.service';
 import { IUser } from '../../shared/interfaces/user.interface';
 import { SocketIoService } from '../../shared/services/socket-io.service';
 import { PaymentLimitsService } from '../../shared/services/payment-limits.service';
+import { takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-settings',
@@ -19,12 +20,9 @@ import { PaymentLimitsService } from '../../shared/services/payment-limits.servi
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit, OnDestroy {
+  private unsubscriber = new Subject();
   showDeleteModal = false;
   passwordSave: string;
-  getSub: Subscription;
-  updSub: Subscription;
-  delSub: Subscription;
-  limSub: Subscription;
 
   delTrue = false;
   updTrue = false;
@@ -48,6 +46,16 @@ export class SettingsComponent implements OnInit, OnDestroy {
     private limitService: PaymentLimitsService
   ) {
     this.getUser();
+
+    this.socketIo
+      .listen('profile')
+      .pipe(
+        takeUntil(this.unsubscriber),
+        tap(() => {
+          this.getUser();
+        })
+      )
+      .subscribe();
   }
 
   ngOnInit(): void {
@@ -74,7 +82,10 @@ export class SettingsComponent implements OnInit, OnDestroy {
       this.user.id = temp;
       this.check();
       this.updTrue = true;
-      this.updSub = this.userService.update(this.user).subscribe();
+      this.userService
+        .update(this.user)
+        .pipe(takeUntil(this.unsubscriber))
+        .subscribe();
       window.alert('Updated Successfully');
       this.socketIo.emit('profile', this.auth.userId);
       this.getUser();
@@ -85,24 +96,33 @@ export class SettingsComponent implements OnInit, OnDestroy {
   }
 
   getUser() {
-    this.getSub = this.userService.getById(this.user.id).subscribe((value) => {
-      this.user = value;
-      this.passwordSave = this.user.password;
-      const split = this.user.fullname.split(' ');
+    this.userService
+      .getById(this.user.id)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((value) => {
+        this.user = value;
+        this.passwordSave = this.user.password;
+        const split = this.user.fullname.split(' ');
 
-      this.form.patchValue({
-        ...this.user,
-        firstName: split[0],
-        lastName: split[1],
+        this.form.patchValue({
+          ...this.user,
+          firstName: split[0],
+          lastName: split[1],
+        });
+        this.userReplicate = this.form.value;
       });
-      this.userReplicate = this.form.value;
-    });
   }
   deleteUser() {
     this.showDeleteModal = false;
     this.delTrue = true;
-    this.delSub = this.userService.delete(this.user.id).subscribe();
-    this.limSub = this.limitService.delete(this.user.id).subscribe();
+    this.userService
+      .delete(this.user.id)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe();
+    this.limitService
+      .delete(this.user.id)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe();
     window.alert('Successfully Deleted');
     this.auth.logout();
   }
@@ -124,15 +144,6 @@ export class SettingsComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.getSub.unsubscribe();
-    if (this.updTrue) {
-      this.updSub.unsubscribe();
-    }
-    if (this.delTrue) {
-      this.delSub.unsubscribe();
-    }
-    if (this.limSub) {
-      this.limSub.unsubscribe();
-    }
+    this.unsubscriber.next();
   }
 }
